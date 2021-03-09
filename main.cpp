1d79d48b31b6e0e6ef4f59e5d9a3f9e31c487649
Resul Çelik <resul_celik@hotmail.com>
2021-03-09 20:11:34 +0100

    Added a parser library that will take care of the parsing of XML files.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "qapplication.h"
 
-int main()
+int main(int argc, char** argv)
 {
+    QApplication app(argc, argv);
+	
     std::cout << "Hello World" << "\n";
 
     return 0;
